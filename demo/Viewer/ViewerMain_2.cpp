32db5a2987bad093f679a71c558ae8565af355ad
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-11-12 10:51:42 +0800

    add demo at 2019-11-12 10:51:42

--- demo/Viewer/ViewerMain_2.cpp
+++ demo/Viewer/ViewerMain_2.cpp
@@ -14,6 +14,11 @@ int main(int, char **) {
         return 1;
     }
 
+    //    void setProjectionMatrix(const osg::Matrix &matrix);
+    //    void setProjectionMatrixAsOrtho(double left, double right, double bottom, double top, double zNear, double zFar);
+    //    void setProjectionMatrixAsOrtho2D(double left, double right, double bottom, double top);
+    //    void setProjectionMatrixAsFrustum(double left, double right, double bottom, double top, double zNear, double zFar);
+    //    void setProjectionMatrixAsPerspective(double fovy, double aspectRatio, double zNear, double zFar);
     viewer.getCamera()->setProjectionMatrixAsPerspective(40., 1., 1., 100.);
     // 创建矩阵，指定到视点的距离。
     osg::Matrix trans;
@@ -21,6 +26,10 @@ int main(int, char **) {
     // 旋转一定角度(弧度值)。
     double angle(0.);
     while (!viewer.done()) {
+        viewer.getCamera()->setClearColor(osg::Vec4(0., 0., 0., 1.)); // 清屏颜色
+        viewer.getCamera()->setClearMask(
+                GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // 清除颜色、深度和模板缓存。
+
         // 创建旋转矩阵。
         osg::Matrix rot;
         rot.makeRotate(angle, osg::Vec3(1., 0., 0.));
